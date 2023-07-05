fc046c32239b2ea44a0fa89b71d5ba54a5568b71
likhitha <likhitha0341@gmail.com>
2023-07-05 23:57:42 +0530

    4

--- hashtable.cpp
+++ hashtable.cpp
@@ -3,37 +3,45 @@
 using namespace std;
 const int SIZE = 10;
 
-class Node {
- public:
+class Node
+{
+public:
   int key;
   int value;
   Node *next;
 
-  Node(int key, int value) {
+  Node(int key, int value)
+  {
     this->key = key;
     this->value = value;
     this->next = nullptr;
   }
 };
 
-class LinkedList {
- public:
+class LinkedList
+{
+public:
   Node *head;
 
-  LinkedList() {
+  LinkedList()
+  {
     head = nullptr;
   }
 
-  void insert(int key, int value) {
+  void insert(int key, int value)
+  {
     Node *new_node = new Node(key, value);
     new_node->next = head;
     head = new_node;
   }
 
-  int search(int key) {
+  int search(int key)
+  {
     Node *current = head;
-    while (current != nullptr) {
-      if (current->key == key) {
+    while (current != nullptr)
+    {
+      if (current->key == key)
+      {
         return current->value;
       }
       current = current->next;
@@ -42,26 +50,8 @@ class LinkedList {
   }
 };
 
-class HashTable {
- public:
-  LinkedList table[SIZE];
-
-  int hash_function(int key) {
-    return key % SIZE;
-  }
-
-  void insert(int key, int value) {
-    int index = hash_function(key);
-    table[index].insert(key, value);
-  }
-
-  int search(int key) {
-    int index = hash_function(key);
-    return table[index].search(key);
-  }
-};
-
-int main() {
+int main()
+{
   HashTable hash_table;
 
   hash_table.insert(1, 100);
@@ -71,6 +61,6 @@ int main() {
   cout << hash_table.search(1) << endl;
   cout << hash_table.search(2) << endl;
   cout << hash_table.search(3) << endl;
-  
+
   return 0;
 }
