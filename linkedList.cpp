a45787beb592ce98eaae40a9aec3969fed711c01
likhitha <likhitha0341@gmail.com>
2023-07-07 18:04:25 +0530

    main no inserber

--- linkedList.cpp
+++ linkedList.cpp
@@ -83,12 +83,44 @@ void Linkedlist::printList()
     }
 }
 
-void concat(Linkedlist l1, Linkedlist l2)
+int main()
 {
-    Node *temp = l1.head;
-    while (temp->next != NULL)
-    {
-        temp = temp->next;
-    }
-    temp->next = l2.head;
+
+    Linkedlist list, list2;
+    list.insertNode(1);
+    list.insertNode(2);
+    list.insertNode(3);
+    list.insertNode(4);
+    cout << "Elements of the list are: ";
+    list.printList();
+    cout << endl;
+    list.deleteNode(2);
+    cout << "Elements of the list are: ";
+    list.printList();
+    list.insertBeg(36);
+    cout << "\n after inserting 36 at begining Elements of the list are: ";
+    list.printList();
+    cout << endl;
+    cout << "\n List 2: ";
+
+    list2.insertNode(11);
+    list2.insertNode(12);
+    list2.insertNode(13);
+    list2.insertNode(14);
+    cout << "Elements of the list are: ";
+    list2.printList();
+    cout << endl;
+    list2.deleteNode(12);
+    cout << "Elements of the list are: ";
+    list2.printList();
+    list2.insertBeg(76);
+    cout << "\n after inserting 36 at begining Elements of the list are: ";
+    list2.printList();
+    cout << endl;
+    list2.search(4);
+    concat(list, list2);
+    cout << "\n after concatination: ";
+    list.printList();
+
+    return 0;
 }
